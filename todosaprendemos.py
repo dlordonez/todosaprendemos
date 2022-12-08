1d37a17c5c8a0b43232208349b035d6ebbee9a64
brandonbedoya <brandon.bedoya@insoftar.com>
2022-12-08 12:32:11 -0500

    Modifica borrar

--- todosaprendemos.py
+++ todosaprendemos.py
@@ -19,6 +19,8 @@ textActividad=Entry(gui)
 textTiempo=Entry(gui)
 # frame es un espacio dentro del gui para ubicar un elemento
 def menu1():
+    labelBorrar=Label(gui,text="")
+    labelBorrar.place(x=0, y=0, width=1000, height=1000)
     frameBotones=Frame(gui)
     B1=Button(frameBotones,text="Programas",command=programas).grid(row=0,column=0)
     B2=Button(frameBotones,text="Estudiantes \n Fonoaudiologia").grid(row=0,column=1)
@@ -33,11 +35,11 @@ def menu2():
     labelBorrar=Label(gui,text="")
     labelBorrar.place(x=60, y=80, width=400, height=300)
     frameBotones2=Frame(gui)
-    B12=Button(frameBotones2,text="2Preguntar").grid(row=0,column=0)
-    B22=Button(frameBotones2,text="2Reporte").grid(row=0,column=1)
-    B32=Button(frameBotones2,text="2Borrar").grid(row=0,column=2)
-    B42=Button(frameBotones2,text="2Leer encuesta").grid(row=0,column=3)
-    B52=Button(frameBotones2,text="2Otro menú", command=menu1).grid(row=0,column=4)
+    B12=Button(frameBotones2,text="2Programas").grid(row=0,column=0)
+    B22=Button(frameBotones2,text="2Estudiantes \n Fonoaudiologia").grid(row=0,column=1)
+    B32=Button(frameBotones2,text="2Estudiantes \n Tedesoft").grid(row=0,column=2)
+    B42=Button(frameBotones2,text="2Reportes").grid(row=0,column=3)
+    B52=Button(frameBotones2,text="2OMejores \n Estudiantes", command=menu1).grid(row=0,column=4)
     frameBotones2.place(x=60, y=20, width=400, height=25)
     gui.update()
 
@@ -78,16 +80,27 @@ def ingresarEncuesta():
     
 def programas():
     labelBorrar=Label(gui,text="")
-    labelBorrar.place(x=100, y=120, width=400, height=260)
-    frameLabels=Frame(gui)
-    labelTdsof=Label(frameLabels,text="TECNOLOGÍA EN DESARROLLO DE SOFTWARE").grid(row=1,column=1)
-    labelFono=Label(frameLabels,text="\n FONOAUDIOLOGÍA").grid(row=1,column=50)
-    labelTdsof2=Label(frameLabels,text="SEMESTRES: 6 \n ASIGNATURAS: Matemáticas, programación").grid(row=3,column=1)
-    labelFono2=Label(frameLabels,text="SEMESTRES: 10 \n ASIGNATURAS: Biología, anatomía").grid(row=3,column=50)
-    B6=Button(gui,text="Ingresar", command=ingresarEncuesta)
-    B6.place(x=160, y=320, width=70, height=30)
-    frameLabels.place(x=100, y=80, width=1300, height=600)
+    labelBorrar.place(x=0, y=0, width=800, height=800)
+    frameLabels1=Frame(gui)
+    frameLabels2=Frame(gui)
+    labelTdsof=Label(frameLabels1,text="TECNOLOGÍA EN DESARROLLO DE SOFTWARE").grid(row=1,column=1)
+    labelFono=Label(frameLabels2,text="\n FONOAUDIOLOGÍA").grid(row=1,column=50)
+    labelTdsof2=Label(frameLabels1,text="SEMESTRES: 6 \n ASIGNATURAS: Matemáticas, programación, ingles").grid(row=3,column=1)
+    #photo1=PhotoImage(file=".gif") # colocar imagen
+    labelFono2=Label(frameLabels2,text="SEMESTRES: 10 \n ASIGNATURAS: Matematicas, Ingles,Biologia").grid(row=3,column=50)
+    #photo2=PhotoImage(file=".gif") #colocar imagen
+    B6=Button(gui,text="Menú Principal", command=menu1)
+    B6.place(x=300, y=320, width=100, height=50,)
+    frameLabels1.place(x=20, y=80, width=1300, height=600)
+    frameLabels2.place(x=480, y=70, width=1300, height=600)
     gui.update()
+
+def EstudiantesFono():
+    labelBorrar=Label(gui,text="")
+    labelBorrar.place(x=0, y=0, width=800, height=800)
+    
+
+    
 menu1()
 gui.mainloop()
 
@@ -110,4 +123,4 @@ gui.mainloop()
 
 
 
-    HOLA MUNDO CON ERRORES
+   
