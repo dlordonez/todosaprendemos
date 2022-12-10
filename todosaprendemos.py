0bdefdd02a6aca2a3addb2fafd7c79998b015f2e
Alexa08rd <ramirezalexandra403@gmail.com>
2022-12-10 14:27:35 -0500

    ingresar funcion para angelica

--- todosaprendemos.py
+++ todosaprendemos.py
@@ -65,32 +65,7 @@ def ingresar():
     B6=Button(gui,text="Ingresar", command=ingresarEncuesta)
     B6.place(x=160, y=320, width=70, height=30)
 
-    
-def updateEncuesta(nuevalinea):
-    print("Actualizando encuesta")
-    arrayLineas=[]
-# enconding manejar caracteres especiales utf-8 es sintaxis
-# [:-1:] quita el salto de linea
-    with open("ANALISIS.txt", "r", encoding="utf-8") as f:
-        for linea in f:
-            arrayLineas.append(linea[:-1:])
-    f.close()
-    f=open("ANALISIS.txt", "w", encoding="utf-8")
-    for j in arrayLineas:
-        f.write(j+"\n")
-    f.write(nuevalinea+"\n")
-    f.close()
-    
-def ingresarEncuesta():
-    actividad=int(textActividad.get())
-    tiempo=int(textTiempo.get())
-    print("Se ingresaran datos")
-    print("Tiempo", tiempo)
-    print("Actividad", actividad)
-    if tiempo > 0 and tiempo <5 and actividad >0 and actividad < 6:
-        updateEncuesta(str(tiempo)+","+str(actividad)+",")
-    else:
-        error()
+
     
 #labelBorrar, sobreescribe, pone una página nueva sobre el gui hay que crear un campo para ingresar texto
     
@@ -151,20 +126,6 @@ def EstudiantesFono():
 menu1()
 gui.mainloop()
 
-'''def programas():
-    global textActividad
-    global textTiempo
-    labelBorrar=Label(gui,text="")
-    labelBorrar.place(x=60, y=80, width=400, height=260)
-    labelTiempo=Label(gui,text="Tiempo libre \n <1> Menos de 3 horas \n <2> Entre 3 y 6 horas\n <3> Entre 6 y 9 horas \n <4> Más de 9 horas")
-    labelTiempo.place(x=100, y=80, width=150, height=80)
-    textTiempo=Entry(gui)
-    textTiempo.place(x=280, y=80, width=150, height=80)
-    labelActividad=Label(gui,text="Actividad principal \n <1> Leer \n <2> Ver televisión \n <3> Hacer deporte \n <4> Dormir \n <5> Otra")
-    labelActividad.place(x=100, y=200, width=150, height=100)
-    textActividad=Entry(gui)
-    textActividad.place(x=280, y=200, width=150, height=80)
-    B6=Button(gui,text="Ingresar", command=ingresarEncuesta)
-    B6.place(x=160, y=320, width=70, height=30)'''
+
 
 
